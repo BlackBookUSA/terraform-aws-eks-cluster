702c3a1f572bce60ad3c3907e4a0f3d666734d85
rparsonsbb <richard.parsons@blackbook.com>
2019-08-22 22:38:08 -0400

    one more list (subnet blah blah)

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "aws_eks_cluster" "default" {
 
   vpc_config {
     security_group_ids      = [join("", aws_security_group.default.*.id)]
-    subnet_ids              = [var.subnet_ids]
+    subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
   }
